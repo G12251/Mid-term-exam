c4919c5a46b19d48344bd574a957c828a89e1438
G12251 <gulnozasaparmatova@gmail.com>
2023-04-24 19:42:15 +0500

    responsive-1

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
 
 // When the user scrolls down 50px from the top of the document, resize the header's font size
-window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
+document.addEventListener("scroll", () => {
   if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
-    document.getElementById("navigator").style.fontSize = "30px";
+    document.querySelector(".nav-bar").style.fontSize = "12px";
   } else {
-    document.getElementById("navigator").style.fontSize = "40px";
+    document.querySelector(".nav-bar").style.fontSize = "14px";
   }
-}
+})
+
+const burgerIcon = document.querySelector("[data-burger]");
+const mobileNavbar = document.querySelector("[data-mobile-navbar]");
+
+burgerIcon.addEventListener("click", () => {
+  mobileNavbar.classList.toggle("open")
+})
+
